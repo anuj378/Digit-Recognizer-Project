6a757c1c07731bec29048f9d9a95ebbcf6fa5e30
Anuj Pareek <anujpareek378@gmail.com>
2019-06-26 14:34:27 +0530

    [SECOND COMMIT] Adding project done till 25th June,19 - (DATA RECEIVED! AT BACKEND) UI improved , Backend Connected , Decoding at backend remaining , Predction at backend remaining

--- demo.py
+++ demo.py
@@ -5,7 +5,12 @@ Created on Fri Jun 21 08:46:23 2019
 @author: King23
 """
 
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import numpy as np
+import pandas as pd
+import PIL as pil
+#import keras
+
 
 app=Flask(__name__)
 
@@ -18,6 +23,17 @@ def showLinks():
 @app.route('/devs')
 def showDevs():
 	return render_template("devs.html")
+@app.route('/predict',methods=['POST'])
+def predict():
+	data=request.form
+	print("IMAGE DATA----->",data['value'])
+	#print("DATA RECEVIED!",value.getlist('image_array'))
+	#print("THE TYPE IS :",type(value['image_array']))
+	
+	if(data):
+		return ("DATA RECEIVED!")
+	else:
+		return ("NO DATA RECEIVED")
 
 
 

43e23c102f85145170e1cf204ea694b62411ad98
Anuj Pareek <anujpareek378@gmail.com>
2019-06-27 13:07:14 +0530

    [THIRD COMMIT] (FULLY-FUNCTIONAL) - Work Done till  26th June,19 - All okay just Needs Accuracy Improvements

--- demo.py
+++ demo.py
@@ -9,33 +9,57 @@ from flask import Flask,render_template,request
 import numpy as np
 import pandas as pd
 import PIL as pil
-#import keras
+from PIL import Image
+import keras
+import matplotlib.pyplot as plt
+import base64
+import io
+#importing model
+# load json and create model
+from keras.models import model_from_json
 
+json_file = open('model_files/CNN_Predictor/model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+
+loaded_model = model_from_json(loaded_model_json)
+loaded_model._make_predict_function()
+
+# load weights into new model
+
+loaded_model.load_weights("model_files/CNN_Predictor/model.h5")
+print("Loaded model from disk")
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template("home.html")
+    return render_template("home.html")
 @app.route('/links')
 def showLinks():
-	return render_template("links.html")
+    return render_template("links.html")
 @app.route('/devs')
 def showDevs():
-	return render_template("devs.html")
+    return render_template("devs.html")
 @app.route('/predict',methods=['POST'])
 def predict():
-	data=request.form
-	print("IMAGE DATA----->",data['value'])
-	#print("DATA RECEVIED!",value.getlist('image_array'))
-	#print("THE TYPE IS :",type(value['image_array']))
-	
-	if(data):
-		return ("DATA RECEIVED!")
-	else:
-		return ("NO DATA RECEIVED")
+    data=dict(request.form)
+    data=base64.decodebytes(bytes((data['value'][22:]),'utf-8'))
+    #image =Image.frombytes('L',(28,28),data)
+    image = Image.open(io.BytesIO(data))
+    image=image.convert('L')
+    image.thumbnail((28,28))
+    image=np.array(image)
+    plt.imshow(image)
+    image=image/255
+    image=image.reshape(1,28,28,1)
+    number=np.argmax(loaded_model.predict(image))
+    if(number):
+        return ("PREDICTED NUMBER : "+str(number))
+    else:
+        return ("UNABLE TO PREDICT")
 
 
 
 if __name__=='__main__':
-	app.run(debug=True)
+    app.run()

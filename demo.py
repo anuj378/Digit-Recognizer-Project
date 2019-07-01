40da17abf8887ab0f4bca841c7263b7eb949d6db
Anuj Pareek <anujpareek378@gmail.com>
2019-07-01 14:13:08 +0530

    [FIFTH COMMIT](FULLY-FUNCTIONAL V1.0) Work done till 30th,June,19 - EXPERIENCE LINK NEEDS TO BE UPDATED

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ Created on Fri Jun 21 08:46:23 2019
 
 @author: King23
 """
-
+#importing libraries
 from flask import Flask,render_template,request
 import numpy as np
 from PIL import Image
@@ -23,12 +23,12 @@ loaded_model = model_from_json(loaded_model_json)
 loaded_model.load_weights("model_files/CNN_Predictor/model.h5")
 print("Loaded model from disk")
 
+#make the predict function
 loaded_model._make_predict_function()
 
-
+#Instantiating the class Flask 
+#app is the object that holds our project
 app=Flask(__name__)
-
-
 
 @app.route('/')
 def home():
@@ -43,15 +43,25 @@ def showDevs():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    data=dict(request.form)
+    #Catching the data received 
+    data=(request.form)
+    #Slicing out only the required portion of the data (BASE64 string)
+    #converting the clipped string into byte format as it is the supported format by the method decodebytes()
     data=base64.decodebytes(bytes((data['value'][22:]),'utf-8'))
     image = Image.open(io.BytesIO(data))
+    #converting the image to greyscale
     image=image.convert('L')
+    #resizing with smoothing (ANTIALIAS)
     image=image.resize((28,28),Image.ANTIALIAS)
+    #converting the image to array
     image = np.asarray(image)
+    #dividing each pixel intensity by 255 to apply MINMAX scaling
     image=image.astype('float32')/255
+    #converting the image shape to that of the training data as it is what the model accepts
     image=image.reshape(1,28,28,1)
+    #storing the index of the output array which has the greatest probabilistic value
     number=np.argmax(loaded_model.predict(image))
+    #returning predicted number as a response
     if((type(number)!=None)):
         return ("PREDICTED NUMBER : "+str(number))
     else:
